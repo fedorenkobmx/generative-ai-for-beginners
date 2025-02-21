7edef181211b327c369f22867110a0b2dfd75c18
fedorenkobmx <fedorenkobmx@GMAIL.COM>
2025-02-21 15:40:28 +0000

    practice N2

--- test/test.py
+++ test/test.py
@@ -1,11 +1,12 @@
 import os
 from openai import OpenAI
-from dotenv import load_dotenv
-load_dotenv()
-if "OPENAI_API_KEY" not in os.environ:
-    raise ValueError("Please set OPENAI_API_KEY environment variable")
 
-client = OpenAI()
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("Please set OPENAI_API_KEY environment variable in your GitHub Codespace secrets.")
+
+client = OpenAI(api_key=api_key)
 
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
@@ -17,5 +18,4 @@ completion = client.chat.completions.create(
         }
     ]
 )
-
 print(completion.choices[0].message.content)
